77d5ce8bccaf6d628be3b60901dfd2dcc86661df
Saïfoulaye Diallo <diallocodeur@gmail.com>
2023-02-22 21:58:55 +0000

    spring boot with twilio sms api

--- src/main/java/com/devfun/springboottwillio/twilio/SmsSender.java
+++ src/main/java/com/devfun/springboottwillio/twilio/SmsSender.java
@@ -1,7 +1,5 @@
 package com.devfun.springboottwillio.twilio;
 
-import com.devfun.springboottwillio.twilio.SmsRequest;
-
 public interface SmsSender {
 
     void sendSms(SmsRequest smsRequest);
--- src/main/java/com/devfun/springboottwillio/twilio/TwilioConfiguration.java
+++ src/main/java/com/devfun/springboottwillio/twilio/TwilioConfiguration.java
@@ -1,7 +1,6 @@
 package com.devfun.springboottwillio.twilio;
 
 import lombok.Data;
-import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
